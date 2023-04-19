4f3044534af7e466a369d154a270392758e42141
Andrea Merlino <amerlino@e3r2p4.42roma.it>
2023-04-19 12:49:31 +0200

    split

---
+++ Libft/ft_split.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	char	*j;
+	int		i;
+
+	i = 0;
+	j = (char *) s;
+	if (start > ft_strlen(s) || len > ft_strlen(s))
+		return (NULL);
+	while (*j)
+	{
+		if (i == start)
+		{
+			p = malloc((len +1) * sizeof(char));
+			if (!p)
+				return (NULL);
+			i = 0;
+			while (i++ < len)
+			*p++ = *j++;
+			*p = '\0';
+			return (p - len);
+		}
+		j++;
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	int		last;
+	char	*j;
+
+	i = 0;
+	last = ft_strlen(s1);
+	while (ft_strchr(set, s1[i]) != 0)
+		i++;
+	while (ft_strchr(set, s1[last -1]) != 0)
+		last--;
+	j = ft_substr(s1, i, last - i);
+	return (j);
+}
+
+
+static int word(char const *s, char c)
+{
+	char	*j;
+	int		i;
+	int		a;
+
+	a = 0;
+	i = 0;
+	j = ft_strtrim(s,&c);
+	while(j[a] != '\0')
+	{
+		while(j[a] != c && j[a])
+			a++;
+		i++;
+		while(j[a] == c)
+		a++;
+	}
+	return (i);
+}
+
+char **ft_split(char const *s, char c)
+{
+	int parole;
+	int i;
+	int j;
+	char **split;
+
+	split = ((char**)malloc(i + 1) * sizeof(char*));
+	parole = word(s,c);
+	while(parole < i)
+	{
+		while(!c)
+			j++;
+		split[i] =(char*)malloc(j +1);
+		while (a < j)	
+			split[i][a++] = *s++;
+		
+
+
+
+
+
+
+
+}	
+int main()
+{ 
+	char stringa[] ="aajjjjjaakkkaooaaappaa";
+	char c = 'a';
+	printf("%d\n", ft_split(stringa, c));
+	return(0);
+}
--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,57 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-size_t	ft_strlen(const char *s)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amerlino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:58:49 by amerlino          #+#    #+#             */
+/*   Updated: 2023/04/17 15:58:51 by amerlino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	unsigned int	i;
+	int		i;
+	int		last;
+	char	*j;
 
 	i = 0;
-	while (s[i] != '\0')
+	last = ft_strlen(s1);
+	while (ft_strchr(set, s1[i]) != 0)
 		i++;
-	return (i);
-}
-
-char *ft_strtrim(char const *s1, char const *set)
-{
-	int i;
-	int runner;
-	int last;
-	char *p;
-
-	last = ft_strlen(set);
-    runner = ft_strlen(s1);
-	p = malloc(runner * sizeof(char));
-	i = 0;
-			if ((s1[i] == set[i]) || (s1[runner - (last - i)] == set[i])) 
-			{
-				while ((s1[i] == set[i]) || (s1[runner - (last  - i)] == set[i]))
-						{
-							if (set[i + 1] == '\0')
-							{
-								free(p);
-								p = malloc((runner - last) * sizeof(char));
-								while(s1[i])
-								{
-									if ((s1[0] == set[0]) && ((s1[ runner - last ]) == set[0]))
-									{
-										if(*p < s1[ runner - last])
-											*p++ = s1[i++];
-									}
-									 if (s1[0] == set[0])
-										*p++ = s1[i++];
-									else if ((s1[ runner - last]) == set[0])
-										if(*p < s1[ runner - last])
-											*p++ = s1[i++];
-								}
-							}
-							i++;
-						}
-			}
-			return (p - (runner - last)); 
-}
-int main()
-{
-	char a[] = "oaieri sera sono andatoa";
-	char b[] = "oa";
-	printf("%s\n", ft_strtrim(a,b));
-	return(0);
+	while (ft_strchr(set, s1[last -1]) != 0)
+		last--;
+	j = ft_substr(s1, i, last - i);
+	return (j);
 }
